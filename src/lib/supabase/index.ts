a583f3c2727f3307c314e5d7a1a3f85f38df82ba
Cameron Adams <cameron.adams@hey.com>
2023-04-02 22:10:25 -0500

    refactor: use setInterval for entity spawning

---
+++ src/lib/supabase/index.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+const client = createClient(
+  import.meta.env.VITE_SUPABASE_URL!,
+  import.meta.env.VITE_SUPABASE_KEY!,
+);
+
+export {
+  client,
+}
--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,38 +1,16 @@
 import { FunctionComponent, useEffect } from "react";
-import { Game } from "../game";
+import { GameManager } from "../game/manager";
+
 
 export const GamePage: FunctionComponent = () => {
   useEffect(() => {
     const canvas = document.querySelector<HTMLCanvasElement>("#game-canvas");
-    if (canvas != null) {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
+    if(!canvas) throw new Error("Game Error: No canvas element detected");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-      const ctx = canvas.getContext("2d");
-
-      if (ctx == null) {
-        console.error("Game Error: Canvas 2D Context not found");
-        return;
-      }
-
-      // Customize canvas context 
-      ctx.font = '40px Verdana';
-
-      const game = new Game(canvas);
-
-      const gameLoop = () => {
-        window.requestAnimationFrame(gameLoop);
-        
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        game.update();
-        game.draw(ctx);
-      };
-
-      gameLoop();
-    } else {
-      console.error("Game Error: Couldn't find canvas")
-    }
+    const manager = new GameManager(canvas);
+    manager.start();
   }, []);
 
   return (
