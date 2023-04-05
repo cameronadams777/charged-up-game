44c401e114e608a190c11613ea45e6ef51a2ce71
Cameron Adams <cameron.adams@hey.com>
2023-04-05 10:37:50 -0500

    test: add vercel rewrites

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GamePage } from "./pages/game";
 import { LeaderboardPage } from "./pages/leaderboard";
 import { MainMenuPage } from "./pages/main-menu";
@@ -10,13 +6,13 @@ import { MainMenuPage } from "./pages/main-menu";
 function App() {
   return (
     <Router>
-        <Routes>
-          <Route path="/leaderboard" element={<LeaderboardPage />} />
-          <Route path="/game" element={<GamePage />} />
-          <Route path="/" element={<MainMenuPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/" element={<MainMenuPage />} />
+      </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
